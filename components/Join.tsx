87a844e6e6d94b7d1d6f9b1be644f7888cca4032
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-13 16:17:16 +0100

    suprememods

--- components/Join.tsx
+++ components/Join.tsx
@@ -1,13 +1,13 @@
 function Join() {
   return (
-    <div className="min-h-screen bg-black text-white flex justify-center">
-        <div className="max-w-4xl flex flex-col items-center justify-center space-y-2.5 py-20">
+    <div className="max-h-full bg-black text-white flex justify-center">
+        <div className="max-w-4xl flex flex-col items-center justify-center space-y-2.5 xl:space-y-5 py-20">
             <h1 className="text-4xl font-bold tracking-wider">Join Bankless Research Community</h1>
-            <p className="max-w-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus amet, non massa ultricies amet.</p>
+            <p className="max-w-md xl:text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus amet, non massa ultricies amet.</p>
             <div>
                 <form className="flex">
-                    <input type="text" placeholder="Enter email" className="p-2.5 w-full rounded-l-lg outline-none text-black" />
-                    <button className="bg-red-700 p-2.5 rounded-r-lg">Subscribe</button>
+                    <input type="text" placeholder="Enter email" className="p-2.5 w-[250px] rounded-l-lg outline-none text-black" />
+                    <button className="bg-red-700 p-2.5 rounded-r-lg xl:text-xl">Subscribe</button>
                 </form>
             </div>
         </div>
--- components/SolutionItem.tsx
+++ components/SolutionItem.tsx
@@ -8,11 +8,11 @@ function SolutionItem({ index, title, description }: Props) {
   return (
     <div className="flex space-x-2.5">
         <div>
-            <h1 className="text-2xl font-semibold text-red-700">{index < 10 && "0"}{index + 1}</h1>
+            <h1 className="text-2xl xl:text-4xl font-semibold text-red-700">{index < 10 && "0"}{index + 1}</h1>
         </div>
         <div>
-            <h1 className="text-2xl font-semibold space-y-2">{title}</h1>
-            <p className="text-sm">{description}</p>
+            <h1 className="text-2xl xl:text-4xl font-semibold space-y-2">{title}</h1>
+            <p className="text-sm xl:text-lg">{description}</p>
         </div>
     </div>
   )
