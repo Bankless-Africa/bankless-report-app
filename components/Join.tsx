a635c144e83e425ebb25f7026d0c39e9ce838cf7
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-13 17:09:01 +0100

    suprememods

--- components/Join.tsx
+++ components/Join.tsx
@@ -5,9 +5,9 @@ function Join() {
             <h1 className="text-4xl font-bold tracking-wider">Join Bankless Research Community</h1>
             <p className="max-w-md xl:text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus amet, non massa ultricies amet.</p>
             <div>
-                <form className="flex flex-col space-y-2.5">
-                    <input type="text" placeholder="Enter email" className="p-2.5 w-full sm:w-[250px] rounded-lg sm:rounded-l-lg outline-none text-black" />
-                    <button className="bg-red-700 p-2.5 rounded-lg sm:rounded-r-lg xl:text-xl">Subscribe</button>
+                <form className="flex flex-col sm:flex-row space-y-2.5 sm:space-y-0">
+                    <input type="text" placeholder="Enter email" className="p-2.5 w-full sm:w-[250px] rounded-lg sm:rounded-none sm:rounded-l-lg outline-none text-black" />
+                    <button className="bg-red-700 p-2.5 rounded-lg sm:rounded-none sm:rounded-r-lg xl:text-xl">Subscribe</button>
                 </form>
             </div>
         </div>
