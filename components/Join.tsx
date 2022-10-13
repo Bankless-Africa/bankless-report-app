61aaf00b10445ce8c61a06db951392666a47ad65
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-13 17:05:41 +0100

    suprememods

--- components/Join.tsx
+++ components/Join.tsx
@@ -1,7 +1,7 @@
 function Join() {
   return (
     <div className="max-h-full bg-black text-white flex justify-center">
-        <div className="max-w-4xl flex flex-col items-center justify-center space-y-2.5 xl:space-y-5 py-20 px-5 text-center sm:text-start">
+        <div className="max-w-4xl flex flex-col items-center justify-center space-y-2.5 xl:space-y-5 py-20 px-5 text-center">
             <h1 className="text-4xl font-bold tracking-wider">Join Bankless Research Community</h1>
             <p className="max-w-md xl:text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus amet, non massa ultricies amet.</p>
             <div>
