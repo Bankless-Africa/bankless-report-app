050e9ea8137836dce9c704f1497679fe58b58241
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-10-14 15:26:33 +0100

    suprememods

--- components/Reports.tsx
+++ components/Reports.tsx
@@ -48,7 +48,7 @@ function Reports() {
     <div className="w-full max-h-full bg-black flex justify-center items-center text-white py-20">
       <div className="max-w-4xl xl:max-w-6xl flex flex-col items-center space-y-4 xl:space-y-8 text-center">
           <h1 className="text-3xl xl:text-6xl font-semibold px-2.5">High-Quality Research Reports</h1>
-          <p className="max-w-sm xl:max-w-xl px-2.5 xl:text-2xl">In order to make use of the full potential of a DAO we have tailored our services and offerings in in 3 packages</p>
+          <p className="max-w-sm xl:max-w-xl px-2.5 xl:text-2xl">In order to make use of the full potential of a DAO we have tailored our services and offerings in 3 packages</p>
           <div className="flex space-x-5 max-w-[300px] sm:max-w-lg md:max-w-xl lg:max-w-full overflow-x-scroll scrollbar-hide">
             {
                 cards.map(card => <Card key={card.key} icon={card.icon} title={card.title} description={card.description} url={card.url} />)
